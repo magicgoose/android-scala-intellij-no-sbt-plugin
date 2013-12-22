49d0962a778670dd25de60f4ab3bcaa3e2c34fef
magicgoose <magic-goose@yandex.ru>
2013-12-22 17:04:09 +0400

    remove delimited continuations support

--- src/com/example/android_scala/MyActivity.scala
+++ src/com/example/android_scala/MyActivity.scala
@@ -27,87 +27,58 @@ package com.example.android_scala
 import android.app.Activity
 import android.os.Bundle
 import android.util.Log
-import scala.util.continuations._
 
-class MyActivity extends Activity
-{
-    /**
-     * Test random scala stuff and see if it works on Android
-     */
-    override def onCreate(savedInstanceState: Bundle)
-    {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.main)
+class MyActivity extends Activity {
+  /**
+   * Test random scala stuff and see if it works on Android
+   */
+  override def onCreate(savedInstanceState: Bundle) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.main)
 
-        val a = new Complex(4.0, 5.0)
-        val b = new Complex(2.0, 3.0)
-        Log.d("MyActivity", "Printing scala test stuff")
-        Log.d("MyActivity", (a + b).toString)
-        Log.d("MyActivity", (a - b).toString)
-        Log.d("MyActivity", a.toString)
-        Log.d("MyActivity", b.toString)
-        Log.d("MyActivity", baz().toString)
-        Log.d("MyActivity", test().toString)
-        Log.d("MyActivity", fun())
-    }
+    val a = new Complex(4.0, 5.0)
+    val b = new Complex(2.0, 3.0)
+    Log.d("MyActivity", "Printing scala test stuff")
+    Log.d("MyActivity", (a + b).toString)
+    Log.d("MyActivity", (a - b).toString)
+    Log.d("MyActivity", a.toString)
+    Log.d("MyActivity", b.toString)
+    Log.d("MyActivity", baz().toString)
+    Log.d("MyActivity", test().toString)
+    Log.d("MyActivity", fun())
+  }
 
 
-    def fun() =
-    {
-        Log.d("MyActivity", "Not part of reset and so not part of the reified function.")
-        reset {
-            shift {
-                // yes, the ⇒ actually works.
-                k: (Int ⇒ Int) ⇒ // Reify reset body into Int => Int function
-                    Log.d("MyActivity", "Calling reified function with value 2")
-                    k(2)
-            } * 300 // reset body _ * 300
-        }
+  def fun() = {
+    "ololo" + 42
+  }
 
-        Log.d("MyActivity", "Outside of reset. Thus not part of the delimited continuation")
+  def test(): Int = {
+    42
+  }
 
-        "Delimited continuations are ridiculous" // return value of fun
-    }
+  def foo() = {
+    println("Once here.")
+    2
+  }
 
-    def test(): Int =
-    {
-        reset {
-            shift {
-                k: (Int ⇒ Int) ⇒
-                    k(k(k(7)))
-            } + 1
-        } * 2
-    }
+  def bar() = {
+    1 + foo()
+  }
 
-    def foo() =
-    {
-        println("Once here.")
-        shift((k: Int ⇒ Int) ⇒ k(k(k(7))))
-    }
+  def baz() = {
+  }
 
-    def bar() =
-    {
-        1 + foo()
-    }
+  class Complex(val real: Double, val imag: Double) {
 
-    def baz() =
-    {
-        reset(bar() * 2)
-    }
+    def +(that: Complex) =
+      new Complex(this.real + that.real, this.imag + that.imag)
 
+    def -(that: Complex) =
+      new Complex(this.real - that.real, this.imag - that.imag)
 
-    class Complex(val real: Double, val imag: Double)
-    {
+    override def toString = real + " + " + imag + "i"
 
-        def +(that: Complex) =
-            new Complex(this.real + that.real, this.imag + that.imag)
-
-        def -(that: Complex) =
-            new Complex(this.real - that.real, this.imag - that.imag)
-
-        override def toString = real + " + " + imag + "i"
-
-    }
-
+  }
 
 }
